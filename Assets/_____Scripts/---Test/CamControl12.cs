e3efb554b749a67bf3b8d23b2cf79a0d4c54c0de
rkaqh232 <kym232@naver.com>
2016-11-20 20:19:48 +0900

    no message

---
+++ Assets/_____Scripts/---Test/CamControl12.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamControl : MonoBehaviour {
+
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+      
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = Player.transform.position;
+        transform.position = new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z);
+    }
+}
