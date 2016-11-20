389cfabba7c332fbc5475d4c064cd4ce113c62f0
rkaqh232 <kym232@naver.com>
2016-11-21 02:15:34 +0900

    no message

---
+++ Assets/_____Scripts/Spawn_SafeZone.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn_SafeZone : MonoBehaviour {
+
+    public GameObject[] SpawnPoint_Safezone;
+    public int Spawn_count;
+
+	// Use this for initialization
+	void Start () {
+        shuffle_point();
+        spawn_zone();
+        
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+
+    void shuffle_point(){
+        for (int i = 0; i < SpawnPoint_Safezone.Length; i++)
+        {
+            GameObject target = SpawnPoint_Safezone[i];
+            int randomIndex = Random.Range(i, SpawnPoint_Safezone.Length);
+            SpawnPoint_Safezone[i] = SpawnPoint_Safezone[randomIndex];
+            SpawnPoint_Safezone[randomIndex] = target;
+        }
+        for (int i = 0; i < SpawnPoint_Safezone.Length; i++)
+        {
+            Debug.Log(SpawnPoint_Safezone[i]);
+        }                
+    }
+
+    void spawn_zone()
+    {
+        for (int i = 0; i < Spawn_count; i++)
+        {
+            SpawnPoint_Safezone[i].SetActive(true);
+        }
+    }
+}
