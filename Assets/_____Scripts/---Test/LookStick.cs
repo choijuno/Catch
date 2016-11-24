f135273b705ffeacebb70d380b75b2610426d63a
mo-desktop <momo@gmail.com>
2016-11-25 05:15:34 +0900

    no message

--- Assets/_____Scripts/---Test/LookStick.cs
+++ Assets/_____Scripts/---Test/LookStick.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 public class LookStick : MonoBehaviour {
-
+	public NetworkView nv;
 	public GameObject Stick;
 
 
 	void Start () {
 	
 	}
-
+	[RPC]
 	void Update () {
 		if (Mathf.Abs(Stick.transform.localPosition.x-146.5f) >= 10 || Mathf.Abs(Stick.transform.localPosition.y-140f) >= 10)
 			transform.LookAt (Stick.transform);
--- Assets/_____Scripts/---Test/LookTaget.cs
+++ Assets/_____Scripts/---Test/LookTaget.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class LookTaget : MonoBehaviour {
 	public GameObject lookStick;
+	public NetworkView nv;
 	// Use this for initialization
+	[RPC]
 	void Start () {
 		if (Application.loadedLevelName != "Test1120") {
 			this.enabled = false;
@@ -13,6 +15,7 @@ public class LookTaget : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+	[RPC]
 	void Update () {
 		transform.rotation = new Quaternion (lookStick.transform.rotation.x,lookStick.transform.rotation.y,lookStick.transform.rotation.z,lookStick.transform.rotation.w);
 	}
